33e360b67fa3575d403d83ccf36f27d87fd6240a
Le Nguyen Hoang Phuc <lephuc260205@gmail.com>
2026-07-29 10:04:29 +0700

    fix(*): fix wrong mapping url annation in controller

--- management/src/main/java/org/phuchoang2005/management/DemoController.java
+++ management/src/main/java/org/phuchoang2005/management/DemoController.java
@@ -2,12 +2,12 @@ package org.phuchoang2005.management;
 
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-  @GetMapping("/error")
+  @RequestMapping("/home")
   public ResponseEntity<String> getError() {
     return new ResponseEntity<>("There is no error for this", HttpStatusCode.valueOf(200));
   }
